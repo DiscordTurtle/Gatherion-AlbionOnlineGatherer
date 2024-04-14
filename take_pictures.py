9796fa7823a32317f33b87bf650feec16fd583c3
Cristian Fildan <c.fildan@student.tue.nl>
2024-04-14 22:39:51 +0200

    made take_pictures

--- take_pictures.py
+++ take_pictures.py
@@ -1,11 +1,14 @@
 import torch
 #stone model
-
+import os
 from helper import *
 import time
 import cv2 as cv
 from move import Move
 import keyboard
+import inquirer
+
+
 
 def main():
     
@@ -16,17 +19,50 @@ def main():
     move_albion_to_top_left(Variables.albion_hwnd, Variables.width, Variables.height)
     
     
+    material = ""
+    while not material:
+        material = input("Enter material name (fiber, fish, leather, ore, stone, wood) : ")
+        if material not in ["fiber", "fish", "leather", "ore", "stone", "wood"]:
+            print("Invalid material")
+            material = ""
     while True: 
         screenshot = window_capture.get_screenshot()
                 
         
-        toggle_vision(screenshot, 1)
+        #toggle_vision(screenshot, 1)
+        print("'s' to save, 'q' to quit")
         
+        if keyboard.is_pressed("s"):
+            
+            
+            save_image(material, screenshot)
+    
               
-        if cv.waitKey(25) & 0xFF == ord('q'):
+        if keyboard.is_pressed("q"):
             cv.destroyAllWindows()
             break
         
+
+def save_image(material,screenshot):
+    folder = "input_pictures/input_" + str(material)
+    max = get_max_from_folder(folder)
+    cv.imwrite(folder + f"/{material}_{max}.jpg", screenshot)
+    max += 1
+    print(f"Saved {material}_{max}.jpg")
+    time.sleep(0.2)
+
         
         
+def get_max_from_folder(folder):
+    max = 0
+    for filename in os.listdir(folder):
+            #filename example stone_1.jpg
+            num = int(filename.split("_")[1].split(".")[0])
+            if num > max:
+                max = num
+    print(f"Max: {max}")
+    return max + 1
+    
+    
+    
 main()
